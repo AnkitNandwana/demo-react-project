9c45ca0ca749b1ec58e1e2a7224a63d89dbc3e89
Ankit Nandwana <ankit.nandwana@mintifi.com>
2023-05-28 12:43:28 +0530

    router commented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 
 import Navbar from './components/Navbar.js'
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import React, { useState } from 'react'
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link,
+//   Routes
+// } from "react-router-dom";
 
 
 function App() {
@@ -43,18 +43,20 @@ function App() {
 
   }
   return (
-    <Router>
+    <>
+     {/* <Router> */}
         <Navbar  home = "Home" mode = {mode} toggleMode = {toggleMode}/>
         <Alert alert = {alert}/>
         <div className="container my-3">
-          {/* <TextForm heading = "Enter the Text"/> */}
+          <TextForm heading = "Enter the Text"/>
           {/* <About/> */}
-          <Routes>
+          {/* <Routes>
             <Route path="/about" element={<About />} />
             <Route path="/" element={<TextForm heading="Enter the text to analyze below"/>} />
-          </Routes>
+          </Routes> */}
         </div>
-    </Router>
+    {/* </Router> */}
+    </>
 
   );
 }

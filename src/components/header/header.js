dcfdb8f8bfc7a87e390918da07805cf7b0e63487
Ankit Nandwana <ankit.nandwana@mintifi.com>
2023-05-28 12:30:06 +0530

    first commit

---
+++ src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './header.css'
+
+
+const Header=() => {
+	return (
+		<div className='header_component'> 
+		<div className='header_display'>
+		  <div className='header_color '>Header1
+		  </div>
+
+		</div>
+		</div>
+		)
+}
+
+export default Header;
---
+++ src/components/login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+ function Login(argument) {
+ 	return (
+ 		<div> ankit 1 </div>
+ 		)
+ }
+
+ export default Login;
